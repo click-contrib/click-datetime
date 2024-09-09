1c9eed7aaf3b04783be9628dda54598fd6f148aa
Dawson R <105720-ddaws@users.noreply.gitlab.com>
2024-09-10 07:03:29 +0800

    Updated example formatting

--- examples/foobar.py
+++ examples/foobar.py
@@ -1,16 +1,20 @@
-
-import click
-from click_datetime import Datetime
-
 from datetime import datetime
 
-@click.option('--date', type=Datetime(format='%d/%m/%y'), default=datetime.now(), help='''
-An example parsing and printing a datetime.
-''')
+import click
+
+from click_datetime import Datetime
+
+
+@click.option(
+    "--date",
+    type=Datetime(format="%d/%m/%y"),
+    default=datetime.now(),
+    help="An example parsing and printing a datetime.",
+)
 @click.command()
 def foobar(date):
-    click.echo('The date : {0}'.format(date))
+    click.echo("The date : {0}".format(date))
 
 
-if __name__ == '__main__':
-    foobar()
+if __name__ == "__main__":
+    foobar()  # type: ignore
